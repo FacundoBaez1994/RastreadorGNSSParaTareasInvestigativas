8414892ed224a880be715ff3537925ca8b208f5e
FacundoBaez1994 <fbaez@fi.uba.ar>
2024-09-01 17:05:27 +0000

    cambio en initMPU9250

--- Modules/InertialSensor/InertialSensor.cpp
+++ Modules/InertialSensor/InertialSensor.cpp
@@ -287,56 +287,52 @@ void InertialSensor::initAK8963() {
 
 
 void InertialSensor::initMPU9250() {  
-     // Initialize MPU9250 device
-    // wake up device
-    this->writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
-    wait_us(100000); //  wait(0.1); Delay 100 ms for PLL to get established on x-axis gyro; should check for PLL ready interrupt  
+       // Initialize MPU9250 device
+        // wake up device
+        writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors
+        wait_us(100000); // Delay 100 ms for PLL to get established on x-axis gyro; should check for PLL ready interrupt
 
-    // get stable time source
-    this->writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);  // Set clock source to be PLL with x-axis gyroscope reference, bits 2:0 = 001
+        // get stable time source
+        writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);    // Set clock source to be PLL with x-axis gyroscope reference, bits 2:0 = 001
 
-    // Configure Gyro and Accelerometer
-    // Disable FSYNC and set accelerometer and gyro bandwidth to 44 and 42 Hz, respectively; 
-    // DLPF_CFG = bits 2:0 = 010; this sets the sample rate at 1 kHz for both
-    // Maximum delay is 4.9 ms which is just over a 200 Hz maximum rate
-    this->writeByte(MPU9250_ADDRESS, CONFIG, 0x03);  
- 
-    // Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
-    this->writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x04);  // Use a 200 Hz rate; the same rate set in CONFIG above
-    
-    // Set gyroscope full scale range
-    // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are left-shifted into positions 4:3
-    uint8_t c = this->readByte(MPU9250_ADDRESS, GYRO_CONFIG); // get current GYRO_CONFIG register value
-    // c = c & ~0xE0; // Clear self-test bits [7:5] 
-    c = c & ~0x02; // Clear Fchoice bits [1:0] 
-    c = c & ~0x18; // Clear AFS bits [4:3]
-    c = c | this->Gscale << 3; // Set full scale range for the gyro
-    // c =| 0x00; // Set Fchoice for the gyro to 11 by writing its inverse to bits 1:0 of GYRO_CONFIG
-    this->writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c ); // Write new GYRO_CONFIG value to register
-    
-    // Set accelerometer full-scale range configuration
-    c = this->readByte(MPU9250_ADDRESS, ACCEL_CONFIG); // get current ACCEL_CONFIG register value
-    // c = c & ~0xE0; // Clear self-test bits [7:5] 
-    c = c & ~0x18;  // Clear AFS bits [4:3]
-    c = c | this->Ascale << 3; // Set full scale range for the accelerometer 
-    this->writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c); // Write new ACCEL_CONFIG register value
+        // Configure Gyro and Accelerometer
+        // Disable FSYNC and set accelerometer and gyro bandwidth to 44 and 42 Hz, respectively;
+        // DLPF_CFG = bits 2:0 = 010; this sets the sample rate at 1 kHz for both
+        // Maximum delay is 4.9 ms which is just over a 200 Hz maximum rate
+        writeByte(MPU9250_ADDRESS, CONFIG, 0x03);
 
-    // Set accelerometer sample rate configuration
-    // It is possible to get a 4 kHz sample rate from the accelerometer by choosing 1 for
-    // accel_fchoice_b bit [3]; in this case the bandwidth is 1.13 kHz
-    c = this->readByte(MPU9250_ADDRESS, ACCEL_CONFIG2); // get current ACCEL_CONFIG2 register value
-    c = c & ~0x0F; // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])  
-    c = c | 0x03;  // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
-    this->writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c); // Write new ACCEL_CONFIG2 register value
+        // Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
+        writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x04);    // Use a 200 Hz rate; the same rate set in CONFIG above
 
-    // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates, 
-    // but all these rates are further reduced by a factor of 5 to 200 Hz because of the SMPLRT_DIV setting
+        // Set gyroscope full scale range
+        // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are left-shifted into positions 4:3
+        uint8_t c = readByte(MPU9250_ADDRESS, GYRO_CONFIG);
+        writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c & ~0xE0); // Clear self-test bits [7:5]
+        writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
+        writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c | this->Mscale << 3); // Set full scale range for the gyro
 
-    // Configure Interrupts and Bypass Enable
-    // Set interrupt pin active high, push-pull, and clear on read of INT_STATUS, enable I2C_BYPASS_EN so additional chips 
-    // can join the I2C bus and all can be controlled by the STM32 as master
-    this->writeByte(MPU9250_ADDRESS, INT_PIN_CFG, 0x22);    
-    this->writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x01);  // Enable data ready (bit 0) interrupt
+        // Set accelerometer configuration
+        c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG);
+        writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c & ~0xE0); // Clear self-test bits [7:5]
+        writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
+        writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c | this->Ascale << 3); // Set full scale range for the accelerometer
+
+        // Set accelerometer sample rate configuration
+        // It is possible to get a 4 kHz sample rate from the accelerometer by choosing 1 for
+        // accel_fchoice_b bit [3]; in this case the bandwidth is 1.13 kHz
+        c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG2);
+        writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c & ~0x0F); // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])
+        writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c | 0x03); // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
+
+        // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates,
+        // but all these rates are further reduced by a factor of 5 to 200 Hz because of the SMPLRT_DIV setting
+
+        // Configure Interrupts and Bypass Enable
+        // Set interrupt pin active high, push-pull, and clear on read of INT_STATUS, enable I2C_BYPASS_EN so additional chips
+        // can join the I2C bus and all can be controlled by the Arduino as master
+        writeByte(MPU9250_ADDRESS, INT_PIN_CFG, 0x22);
+        writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x01);    // Enable data ready (bit 0) interrupt
+        wait_us (100000); // wait for pass-through mode enabled
 }
 
 // Function which accumulates gyro and accelerometer data after device initialization. It calculates the average
@@ -415,6 +411,7 @@ void InertialSensor::calibrateMPU9250( ) {
         
     if(accel_bias[2] > 0L) {accel_bias[2] -= (int32_t) accelsensitivity;}  // Remove gravity from the z-axis accelerometer bias calculation
     else {accel_bias[2] += (int32_t) accelsensitivity;}
+    // el bias deberia de sacarse de todos los ejes si se supera un cierto valor?
     
     // Construct the gyro biases for push to the hardware gyro bias registers, which are reset to zero upon device startup
     data[0] = (-gyro_bias[0]/4  >> 8) & 0xFF; // Divide by 4 to get 32.9 LSB per deg/s to conform to expected bias input format
@@ -519,10 +516,12 @@ void InertialSensor::readAccelData() {
     this->accelCount[0] = (int16_t)(((int16_t)rawData[0] << 8) | rawData[1]) ;  // Turn the MSB and LSB into a signed 16-bit value
     this->accelCount[1] = (int16_t)(((int16_t)rawData[2] << 8) | rawData[3]) ;  
     this->accelCount[2] = (int16_t)(((int16_t)rawData[4] << 8) | rawData[5]) ; 
-
-    this->ax = (float) this->accelCount[0]*this->aRes - this->accelBias[0];  // get actual g value, this depends on scale being set
-    this->ay = (float) this->accelCount[1]*this->aRes - this->accelBias[1];   
-    this->az = (float) this->accelCount[2]*this->aRes - this->accelBias[2]; 
+    this->ax = (float) accelCount[0] * this->aRes;
+    this->ay = (float) accelCount[1] * this->aRes;
+    this->az = (float) accelCount[2] * this->aRes;
+    //this->ax = (float) this->accelCount[0] * this->aRes - this->accelBias[0];  // get actual g value, this depends on scale being set
+    //this->ay = (float) this->accelCount[1] * this->aRes - this->accelBias[1];   
+    //this->az = (float) this->accelCount[2] * this->aRes - this->accelBias[2]; 
 
     /////
     sprintf(buffer, "ax = %f\n\r",  this->ax);
